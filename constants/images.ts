8495d7f15bf8a6aaa450687fc9faa8f6ded86a77
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2025-01-28 00:25:05 -0500

    google auth and image assets

---
+++ constants/images.ts
@@ -0,0 +1,25 @@
+import onboarding from "@/assets/images/onboarding.png";
+import loginOnboarding from "@/assets/images/login_onboarding.png";
+import avatar from "@/assets/images/avatar.png";
+import newYork from "@/assets/images/new-york.png";
+import japan from "@/assets/images/japan.png";
+import cardGradient from "@/assets/images/card-gradient.png";
+import barChart from "@/assets/images/bar-chart.png";
+import whiteGradient from "@/assets/images/white-gradient.png";
+import map from "@/assets/images/map.png";
+import noResult from "@/assets/images/no-result.png";
+import booral from "@/assets/images/icon.png";
+
+export default {
+    onboarding,
+    loginOnboarding,
+    avatar,
+    newYork,
+    japan,
+    cardGradient,
+    barChart,
+    whiteGradient,
+    map,
+    noResult,
+    booral
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          100: "#fedf62",
+          200: "#fdc52a",
+          300: "#c87b00",
+        },
+        accent: {
+          100: "#FBFBFD",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191D31",
+        },
+        danger: "#F75555",
+      },
+    },
   },
   plugins: [],
 }
